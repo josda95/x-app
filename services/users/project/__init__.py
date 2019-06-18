7459a678ad48ab26f4246f1c869c0436c594481a
josda95 <josuehuanca@upeu.edu.pe>
2019-06-17 20:09:16 -0500

    refactorizacion

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,17 @@
 # services/users/project/__init__.py
 
-
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension  # nuevo
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
+from flask_migrate import Migrate  # nuevo
 
 # instanciado la db
 db = SQLAlchemy()
-toolbar = DebugToolbarExtension()  # nuevo
+toolbar = DebugToolbarExtension()
+cors = CORS()
+migrate = Migrate()  # nuevo
 
 
 # nuevo
@@ -22,7 +25,9 @@ def create_app(script_info=None):
 
     #  estableciendo extextensiones
     db.init_app(app)
-    toolbar.init_app(app)  # nuevo
+    toolbar.init_app(app)
+    cors.init_app(app)
+    migrate.init_app(app, db)  # nuevo
 
     # registrando blueprints
     from project.api.users import users_blueprint
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -7,6 +7,7 @@ import unittest
 from project.tests.base import BaseTestCase
 from project import db
 from project.api.models import User
+from project.tests.utils import add_user
 
 
 def add_user(username, email):
